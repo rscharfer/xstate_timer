94c69d52877d966b3c285adc2fef19574d0205b7
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-08-05 13:16:13 +0200

    added null event

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
 import { createMachine, interpret, assign } from "xstate";
 
-const MAX_DURATION = 30000;
-
 const resetButton = document.querySelector("#reset");
 const slider = document.querySelector("#slider");
 const output = document.querySelector("#output");
 const guage = document.querySelector("#guage");
 
-const machine = createMachine(
-  {
-    initial: "active",
-    context: {
-      elapsedTime: 0,
-      max: 3.0,
-    },
-    states: {
-      active: {
-        invoke: {
-          id: "addTenth",
-          src: (ctx, event) => (callback, onReceive) => {
-            const id = setInterval(() => callback("ADD_TENTH"), 100);
-            return () => clearInterval(id);
-          },
-        },
-        on: {
-          ADD_TENTH: [
-            {
-              cond: (ctx, event) => (ctx.elapsedTime < 30 ? false : true),
-              target: "elapsed",
-            },
-            {
-              actions: assign({
-                elapsedTime: (ctx, event) => {
-                  const next = ctx.elapsedTime + 0.1;
-                  return +next.toFixed(2);
-                },
-              }),
-            },
-          ],
-          RESET: {
-            actions: assign({ elapsedTime: 0 }),
-          },
-          SET_MAX: {
-            actions: assign({ max: (c, e) => e.value }),
-          },
+const MAX_DURATION = 30; // in seconds
+
+const machine = createMachine({
+  initial: "active",
+  context: {
+    clock: 0,
+    max: 15.0,
+    interval: 100, // in milliseconds
+  },
+  states: {
+    active: {
+      // invoke an actor which sends events back to parent at regular intervals
+      // the interval will be cleared when the state transitions away from active
+      invoke: {
+        src: (ctx) => (callback) => {
+          const id = setInterval(() => callback("TICK"), ctx.interval);
+          return () => clearInterval(id);
         },
       },
-      elapsed: {
-        on: {
-          RESET: {
-            actions: assign({ elapsedTime: 0 }),
-            target: "active",
-          },
-          SET_MAX: {
-            actions: assign({ max: (c, e) => e.value }),
-          },
+      on: {
+        // null event
+        // whenever the state is transitioned to, check to see if we need to move to a different state
+        "": {
+          cond: (ctx) => (ctx.clock >= MAX_DURATION ? true : false),
+          target: "elapsed",
+        },
+        // when the parent receives a TICK from the actor,
+        // parent will update its context
+        TICK: {
+          actions: assign({
+            clock: (ctx) => +(ctx.clock + ctx.interval / 1000).toFixed(1), // add .1 to the current value + make it a string with two places, convert it back to a number
+          }),
+        },
+        // reset the clock on RESET
+        // max will stay the same
+        RESET: {
+          actions: assign({ clock: 0 }),
+        },
+        // set max on range input update
+        SET_MAX: {
+          actions: assign({ max: (_, e) => e.value }),
+        },
+      },
+    },
+    // this state is really only here to clean up the interval
+    elapsed: {
+      on: {
+        RESET: {
+          actions: assign({ clock: 0 }),
+          target: "active",
+        },
+        SET_MAX: {
+          actions: assign({ max: (c, e) => e.value }),
         },
       },
     },
   },
-  {}
-);
+});
 
 const service = interpret(machine).start();
 service.onTransition((state) => {
-  const { elapsedTime, max } = state.context;
-  console.log("max is", max);
+  const { clock, max } = state.context;
 
-  if (elapsedTime >= max) {
+  if (clock >= max) {
     output.innerHTML = `${max}s`;
     guage.style.width = "100%";
   } else {
-    output.innerHTML = `${elapsedTime}s`;
-    guage.style.width = `${(elapsedTime / max) * 100}%`;
+    output.innerHTML = `${clock}s`;
+    guage.style.width = `${(clock / max) * 100}%`;
   }
 });
 
